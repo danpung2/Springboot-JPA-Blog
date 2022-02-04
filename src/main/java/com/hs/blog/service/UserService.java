cb8bfae9e0f221505aa5977313d67e2d80730699
danpung2 <danpung_2@naver.com>
2022-02-04 19:37:06 +0900

    CREATE traditional login

--- src/main/java/com/hs/blog/service/UserService.java
+++ src/main/java/com/hs/blog/service/UserService.java
@@ -4,8 +4,8 @@ import com.hs.blog.model.User;
 import com.hs.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 // 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌(IoC를 해준다)
 @Service
@@ -15,14 +15,12 @@ public class UserService {
     private UserRepository userRepository;
 
     @Transactional
-    public int 회원가입(User user){
-        try{
-            userRepository.save(user);
-            return 1;
-        } catch (Exception e){
-            e.printStackTrace();;
-            System.out.println("UserService: 회원가입(): " + e.getMessage());
-        }
-        return -1;
+    public void 회원가입(User user){
+        userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true) // Select 할 때 트랜잭션 시작, 서비스 종료 시에 트랜잭션 종료 (정합성)
+    public User 로그인(User user){
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
     }
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,8 +3,10 @@ let index = {
         $("#btn-save").on("click", ()=>{ // function(){} 대신 ()=>{} 로 사용하는 이유는 this를 바인딩 하기 위해서임
             this.save();
         });
+        $("#btn-login").on("click", ()=>{
+            this.login();
+        });
     },
-
     save: function(){
         let data = {
             username: $("#username").val(),
@@ -24,6 +26,30 @@ let index = {
         }).done(function (response){
             console.log(response);
             alert("회원가입이 완료되었습니다.");
+            location.href = "/blog";
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        }); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+    },
+
+    login: function(){
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val()
+        };
+        // console.log(data);
+
+        // ajax 호출 시 default가 비동기 호출
+        $.ajax({
+            type: "POST",
+            url: "/blog/api/user/login",
+            data: JSON.stringify(data), // http body 데이터
+            contentType: "application/json;charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
+            dataType: "json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 String인데 생긴게 json 이라면 javascript 오브젝트로 변경해줌
+            // 회원가입 수행 요청
+        }).done(function (response){
+            console.log(response);
+            alert("로그인이 완료되었습니다.");
             location.href = "/blog";
         }).fail(function (error){
             alert(JSON.stringify(error));
